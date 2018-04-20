4db9d5542548f33107f220df7e55c6d92e69953d
Bradley Kimmance <bradman42@hotmail.com>
2018-04-20 16:37:53 +0100

    Added Customers Page and Form / Redirect

--- app.js
+++ app.js
@@ -1,22 +1,50 @@
+//Frameworks and Libraries
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser")
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
+var customers = [
+    {name: "Brad", town: "hitchin"},
+    {name: "Dan", town: "hitchin"},
+    {name: "John", town: "luton"}
+];
+
+//Homepage / Login
 app.get("/", function(req, res){
     res.render("login")
 });
 
-app.get("/user", function(req, res){
-    var users = [
-        {name: "Brad", town: "hitchin"},
-        {name: "Dan", town: "hitchin"},
-        {name: "John", town: "luton"}
-    ]
+// User Homepage
+app.get("/user", function(req, res) {
 
-    res.render("user/index", {users:users});
+    res.render("user/index", {customers:customers});
 });
 
+//Customer Page
+app.get("/customers", function(req, res) {
+    res.render("user/customers", {customers:customers});
+});
+
+// New Customer Post Request
+app.post("/customers", function(req, res){
+    var name = req.body.name;
+    var town = req.body.town;
+    var newCustomer = {name: name, town: town};
+    customers.push(newCustomer);
+
+    //Redirect back to customers page
+    res.redirect("/customers");
+});
+
+//New Customer Form Page
+app.get("/customers/new", function(req, res){
+    res.render("user/newcustomer.ejs");
+});
+
+//Start Server on Port 3000
 app.listen(3000, process.env.IP, function(){
     console.log("The Server Has Started.");
 });
